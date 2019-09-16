039b25da159f765cf53c1360060c6640d15cd230
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-16 10:14:25 -0700

    working on compareSync

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let {username, password} = req.body;
 
-  Users.add(user)
+const hash = bcrypt.hashSync(password, 8)
+
+  Users.add({username, password: hash})
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -34,8 +36,8 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        if(bcrypt.compareSync(password, user.password)){
+        res.status(200).json({ message: `Welcome ${user.username}!` })
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -59,8 +61,8 @@ server.get('/hash', (req, res) => {
 
  // hash the name
   const hash = bcrypt.hashSync(name, 12) //use bcrypt
-  name = hash
- res.send(`the hash for ${name} is `)
+  
+ res.send(`the hash for ${name} is ${hash} `)
 })
 
 const port = process.env.PORT || 5000;
